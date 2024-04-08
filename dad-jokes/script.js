586a0a26da64aa72ccae211e8ddb8013c4761204
Raju Chougale <rjchgl08@gmail.com>
2024-04-08 09:51:11 +0530

    js logic added using async/await

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -5,17 +5,32 @@ jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke();
 
-// USING .then()
-function generateJoke() {
+// USING async/await
+async function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
     },
   };
 
-  fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke;
-    });
+  const res = await fetch('https://icanhazdadjoke.com', config);
+
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
 }
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
